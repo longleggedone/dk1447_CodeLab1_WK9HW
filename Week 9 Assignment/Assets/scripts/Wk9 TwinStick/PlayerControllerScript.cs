22b8bd6b58184ba827b6bae56c9c773377faefa8
Daniel Kimsey <longlegsclassic@gmail.com>
2017-04-03 21:24:27 -0400

    Added Player Control
    move and rotate

---
+++ Week 9 Assignment/Assets/scripts/Wk9 TwinStick/PlayerControllerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody))]
+public class PlayerControllerScript : MonoBehaviour {
+
+	Vector3 velocity;
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	public void Move (Vector3 passedVelocity){
+		velocity = passedVelocity;
+	}
+
+	public void LookAt(Vector3 point){
+		Vector3 correctPoint = new Vector3(point.x, transform.position.y, point.z);
+		transform.LookAt (correctPoint);
+	}
+
+	public void FixedUpdate(){
+		rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+	}
+}
---
+++ Week 9 Assignment/Assets/scripts/Wk9 TwinStick/PlayerScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (PlayerControllerScript))]
+public class PlayerScript : MonoBehaviour {
+
+	public float movementSpeed = 10;
+
+	Camera camera;
+	PlayerControllerScript controller;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<PlayerControllerScript>();
+		camera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 movementInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+		Vector3 movementVelocity = movementInput.normalized * movementSpeed;
+		controller.Move(movementVelocity);
+
+		Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+		Plane ground = new Plane(Vector3.up, Vector3.zero);
+		float rayDistance;
+
+		if (ground.Raycast(ray, out rayDistance)){
+			Vector3 point = ray.GetPoint(rayDistance);
+			Debug.DrawLine(ray.origin, point, Color.red);
+			controller.LookAt(point);
+		}
+
+	}
+}
