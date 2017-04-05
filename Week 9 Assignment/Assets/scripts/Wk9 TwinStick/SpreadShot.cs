363013ca3c8b33f4955694e4a0b8e0d0bdeca437
Daniel Kimsey <longlegsclassic@gmail.com>
2017-04-05 07:22:20 -0400

    Fixed Spread Shot

--- Week 9 Assignment/Assets/scripts/Wk9 TwinStick/SpreadShot.cs
+++ Week 9 Assignment/Assets/scripts/Wk9 TwinStick/SpreadShot.cs
@@ -6,8 +6,11 @@ public class SpreadShot : GunScript {
 
 
 
-	public float spreadAngle1;
-	public float spreadAngle2;
+	public float spreadAngle1 = 45;
+	public float spreadAngle2 = 315;
+
+	public Transform projStartPointLeft;
+	public Transform projStartPointRight;
 
 	public void Start(){
 		
@@ -18,8 +21,8 @@ public class SpreadShot : GunScript {
 		if (Time.time > shotTimer){
 			shotTimer = Time.time + fireInterval / 1000;
 			FireProjectile(projStartPoint.position, projStartPoint.rotation);
-			FireProjectile(projStartPoint.position, projStartPoint.rotation * Quaternion.Euler(0,45,0));
-			FireProjectile(projStartPoint.position, projStartPoint.rotation * Quaternion.Euler(0,315,0));
+			FireProjectile(projStartPointRight.position, projStartPointRight.rotation * Quaternion.Euler(0,spreadAngle1,0));
+			FireProjectile(projStartPointLeft.position, projStartPointLeft.rotation * Quaternion.Euler(0,spreadAngle2,0));
 		}
 	}
 
